6c436f0393e34d8ce49a9f8486114175f2999feb
grandmastar27 <grandmastar27@gmail.com>
2020-12-06 09:08:37 +0100

    Create halt.asm

---
+++ Assembly-HaltTheSystem/halt.asm
@@ -0,0 +1,17 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here
+
+; Halt the system
+mov ax, 5
+HLT
+
+ret
+
+
+
+
