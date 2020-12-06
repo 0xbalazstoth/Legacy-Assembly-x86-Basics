ade6cfaa6886a95876209f806264753a9cfce7c9
grandmastar27 <grandmastar27@gmail.com>
2020-12-06 09:11:13 +0100

    Update math.asm

--- Assembly-BasicMath/math.asm
+++ Assembly-BasicMath/math.asm
@@ -9,23 +9,30 @@ org 100h
 ; sum
 mov al, 20
 add al, 10
-;
 
 ; subtract
 mov al, 29
 sub al, 2
-;
 
-; multiply
-; mul: unsigned; imul: signed 
+; unsigned multiply 
 mov al, -7
 mov bl, -2
 imul bl
  
-;divide
+; signed multiply
+mov al, -2
+mov bl, -4
+imul bl 
+ 
+; unsigned divide
 mov ax, 20
 mov bl, 10
 div bl
+
+; signed divide
+mov ax, -203
+mov bl, 4
+idiv bl
  
 ret
 
